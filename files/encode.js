1fb1eed72eecafb5ef66a776c97b36b5f596fb8d
insert-text-here <125519936+insert-text-here@users.noreply.github.com>
2023-04-15 21:12:41 -0700

    Update encode.js

--- files/encode.js
+++ files/encode.js
@@ -1,36 +1,20 @@
-javascript:const css=`
-background:linear-gradient(to right,#e6e9f0,#eef1f5);
-padding:10px;
-border-radius:5px;
-box-shadow:0 0 10px rgba(0,0,0,0.5);
-position:fixed;
-top:50px;
-left:50px;
-z-index:99999;
-`;
-const html=`
-<div style='${css}' draggable='true'>
-  <h1 style='font-size:20px;text-align:center;'>Base64 Encoder/Decoder</h1>
-  <textarea id='input' style='width:100%;height:100px;margin-bottom:10px;'></textarea>
-  <textarea id='output' style='width:100%;height:100px;margin-bottom:10px;'></textarea>
-  <div style='text-align:center;'>
-    <button id='encode' style='background-color:#4CAF50;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;margin-right:10px;'>Encode</button>
-    <button id='decode' style='background-color:#008CBA;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;margin-right:10px;'>Decode</button>
-    <button id='copy' style='background-color:#555555;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;margin-right:10px;'>Copy Encoded</button>
-    <button id='delete' style='background-color:#f44336;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;'>Delete</button>
-  </div>
-</div>
-`;
+const css=`background:linear-gradient(to right,#e6e9f0,#eef1f5);padding:10px;border-radius:5px;box-shadow:0 0 10px rgba(0,0,0,0.5);position:fixed;top:50px;left:50px;z-index:99999;`;
+const html=`<div style='${css}'><h1 style='font-size:20px;text-align:center;'>Base64 Encoder/Decoder</h1><textarea id='input' style='width:100%;height:100px;margin-bottom:10px;'></textarea><textarea id='output' style='width:100%;height:100px;margin-bottom:10px;'></textarea><div style='text-align:center;'><button id='encode' style='background-color:#4CAF50;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;margin-right:10px;'>Encode</button><button id='decode' style='background-color:#008CBA;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;margin-right:10px;'>Decode</button><button id='copy' style='background-color:#555555;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;margin-right:10px;'>Copy Encoded</button><button id='delete' style='background-color:#f44336;border:none;color:white;padding:10px 20px;text-align:center;text-decoration:none;display:inline-block;font-size:16px;'>Delete</button></div><div id="resize-handle" style="position:absolute;bottom:0;right:0;width:20px;height:20px;cursor:se-resize;"></div><div id="output-resize-handle" style="position:absolute;bottom:0;right:0;width:20px;height:20px;cursor:se-resize;"></div></div>`;
 document.body.insertAdjacentHTML('afterbegin',html);
 
-const box=document.querySelector('div[draggable="true"]');
+const box=document.querySelector('div');
+const inputBox=document.getElementById('input');
+const outputBox=document.getElementById('output');
+
 let isDragging=false;
 let lastX,lastY;
 
 box.addEventListener('mousedown',(e)=>{
-  isDragging=true;
-  lastX=e.clientX;
-  lastY=e.clientY;
+  if(e.target!==inputBox&&e.target!==document.getElementById('resize-handle')&&e.target!==outputBox&&e.target!==document.getElementById('output-resize-handle')){
+    isDragging=true;
+    lastX=e.clientX;
+    lastY=e.clientY;
+  }
 });
 
 box.addEventListener('mouseup',()=>{
@@ -48,6 +32,56 @@ box.addEventListener('mousemove',(e)=>{
     lastX=e.clientX;
     lastY=e.clientY;
   }
+});
+
+const resizeHandle=document.getElementById('resize-handle');
+
+resizeHandle.addEventListener('mousedown',(e)=>{
+  e.stopPropagation();
+  const startX=e.clientX;
+  const startY=e.clientY;
+  const startWidth=parseInt(document.defaultView.getComputedStyle(box).width,10);
+  const startHeight=parseInt(document.defaultView.getComputedStyle(box).height,10);
+
+  const mouseMoveHandler=(e)=>{
+    const width=startWidth+e.clientX-startX;
+    const height=startHeight+e.clientY-startY;
+    box.style.width=`${width}px`;
+    box.style.height=`${height}px`;
+  };
+
+  const mouseUpHandler=()=>{
+    document.removeEventListener('mousemove',mouseMoveHandler);
+    document.removeEventListener('mouseup',mouseUpHandler);
+  };
+
+  document.addEventListener('mousemove',mouseMoveHandler);
+  document.addEventListener('mouseup',mouseUpHandler);
+});
+
+const outputResizeHandle=document.getElementById('output-resize-handle');
+
+outputResizeHandle.addEventListener('mousedown',(e)=>{
+  e.stopPropagation();
+  const startX=e.clientX;
+  const startY=e.clientY;
+  const startWidth=parseInt(document.defaultView.getComputedStyle(outputBox).width,10);
+  const startHeight=parseInt(document.defaultView.getComputedStyle(outputBox).height,10);
+
+  const mouseMoveHandler=(e)=>{
+    const width=startWidth+e.clientX-startX;
+    const height=startHeight+e.clientY-startY;
+    outputBox.style.width=`${width}px`;
+    outputBox.style.height=`${height}px`;
+  };
+
+  const mouseUpHandler=()=>{
+    document.removeEventListener('mousemove',mouseMoveHandler);
+    document.removeEventListener('mouseup',mouseUpHandler);
+  };
+
+  document.addEventListener('mousemove',mouseMoveHandler);
+  document.addEventListener('mouseup',mouseUpHandler);
 });
 
 const encodeButton=document.getElementById('encode');
