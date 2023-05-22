8a5426a3ff3190f77c0d44fa0dfeca20d1738acb
insert-name-here <125519936+insert-text-here@users.noreply.github.com>
2023-05-22 14:39:39 -0700

    Update gradient.js

--- files/gradient.js
+++ files/gradient.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const hiddenBtn = document.getElementById("hidden-btn");
   document.addEventListener("keydown", function (event) {
     if (event.keyCode === 192) { // keyCode 192 corresponds to the "`" key
-      hiddenBtn.style.display = hiddenBtn.style.display === "none" ? "block" : "none";
+      window.location.href='./new/code.html'
     }
   });
 });
