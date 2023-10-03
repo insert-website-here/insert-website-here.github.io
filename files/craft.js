8c9a28648742bc56a61f217f68b9750c1d10f271
insert-text-here <125519936+bro-my-username-got-blocked@users.noreply.github.com>
2023-10-03 12:28:13 -0700

    Create craft.js

---
+++ files/craft.js
@@ -0,0 +1,1 @@
+javascript:(function(){window.mcbmRootURI='https://insert-website-here.github.io/mc/';window.mcbmScriptURI='mcbm.min.js';window.mcbmLang='eng';var s,ss=window.mcbmRootURI+'js/mcbm-load.min.js';s=document.createElement('script');s.src=ss;document.body.appendChild(s);})();
